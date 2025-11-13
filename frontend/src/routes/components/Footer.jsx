8726e0e0265a7d63d48eb709e610d25f5886a070
aredboy <germanrojo.biologistic@gmail.com>
2025-11-12 23:50:13 -0300

    footer desktop

--- frontend/src/routes/components/Footer.jsx
+++ frontend/src/routes/components/Footer.jsx
@@ -1,20 +1,36 @@
 import { Link, NavLink } from "react-router-dom";
 import '../styles/footer.css'
+import logoIcon from "../../assets/logo group.png"
+import insta from "../../assets/insta.png"
+import whatsapp from "../../assets/whatsa.png"
+import email from "../../assets/email.png"
 
 export const Footer = () => {
     return (
         <>
         <footer className="custom-footer py-4">
-            <div className="container">
                 <div className="row">
+                    <div>
+                        <Link to='/' className="nav-logo">
+                            <img src={logoIcon} alt="logo" className="logo"/>
+                        </Link>
+                    </div>
                     <div className="col-md-4 mb-3">
                         <ul className="list-unstyled">
                             <li>
                                 <NavLink
-                                    to="/contact"
+                                    to="/about"
                                     className="nav-link text-dark text-decoration-none"
                                 >
-                                    CONTACTO
+                                    SOBRE MI
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/products"
+                                    className="nav-link text-dark text-decoration-none"
+                                >
+                                    NUESTROS PRODUCTOS
                                 </NavLink>
                             </li>
                             <li>
@@ -25,27 +41,22 @@ export const Footer = () => {
                                     PREGUNTAS FRECUENTES
                                 </NavLink>
                             </li>
-                            <li>
-                                <NavLink
-                                    to="/login"
-                                    className="nav-link text-dark text-decoration-none"
-                                >
-                                    LOGIN
-                                </NavLink>
-                            </li>
                         </ul>
                     </div>
                     <div className="col-md-4 mb-3">
-                        <h5>Acerca de mí</h5>
-                        <p>
-                            We are a company dedicated to providing the best
-                            products and services.
-                        </p>
-                    </div>
-                    <div className="col-md-4 mb-3">
-                        <h5>Informacion de Contacto</h5>
-                        <p>Email: support@example.com</p>
-                        <p>Teléfono: (123) 456-7890</p>
+                        <h5>CONTACTO</h5>
+                        <Link to='/' className="nav-logo">
+                            <img src={email} alt="email" className="link"/>
+                            <p>support@example.com</p>
+                        </Link>
+                        <Link to='/' className="nav-logo">
+                            <img src={whatsapp} alt="whatsapp" className="link"/>
+                            <p>Teléfono: (123) 456-7890</p>
+                        </Link>
+                        <Link to='https://www.instagram.com/santalicedya/' className="nav-logo">
+                            <img src={insta} alt="instagram" className="link"/>
+                            <p>santalicedya</p>
+                        </Link>
                     </div>
                 </div>
                 <div className="mt-3">
@@ -54,7 +65,6 @@ export const Footer = () => {
                         All rights reserved.
                     </p>
                 </div>
-            </div>
         </footer>
         </>
     );
