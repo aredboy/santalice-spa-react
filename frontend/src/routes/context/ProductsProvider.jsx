00d76733752de7dc511bf1b60aae3823fe088a9c
aredboy <germanrojo.biologistic@gmail.com>
2025-12-09 19:54:46 -0300

    variable de entorno

--- frontend/src/routes/context/ProductsProvider.jsx
+++ frontend/src/routes/context/ProductsProvider.jsx
@@ -1,3 +1,35 @@
+// import { useState, useEffect } from "react"
+// import { ProductsContext } from "./ProductsContext"
+
+// export const ProductsProvider = ({children}) => {
+
+//     const [products, setProducts] = useState([])
+
+//     const fetchProducts = async() => {
+//     try {
+//     const response = await fetch('http://localhost:5000/products')
+//     if(!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`)
+//     }
+//     const data = await response.json()
+//     console.log(data)
+//     setProducts(data)
+//     } catch (error) {
+//         console.log('Error fetching products:', error)
+//     }
+//     }
+
+//     useEffect(() => {
+//     fetchProducts()
+//     }, [])
+    
+//     return (
+//         <ProductsContext.Provider value={{products}}>
+//             {children}
+//         </ProductsContext.Provider>
+//     )
+// }
+
 import { useState, useEffect } from "react"
 import { ProductsContext } from "./ProductsContext"
 
@@ -6,21 +38,30 @@ export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState([])
 
     const fetchProducts = async() => {
-    try {
-    const response = await fetch('http://localhost:5000/products')
-    if(!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-    }
-    const data = await response.json()
-    console.log(data)
-    setProducts(data)
-    } catch (error) {
-        console.log('Error fetching products:', error)
-    }
+        try {
+            // 1. Definimos la URL base. 
+            // Si existe la variable de entorno (en Netlify), usa esa. 
+            // Si no (en tu PC), usa localhost.
+            const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+            
+            console.log("Conectando a:", baseUrl); // Para depurar
+
+            // 2. Hacemos el fetch concatenando la URL base con el endpoint
+            const response = await fetch(`${baseUrl}/products`)
+            
+            if(!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+            const data = await response.json()
+            console.log(data)
+            setProducts(data)
+        } catch (error) {
+            console.log('Error fetching products:', error)
+        }
     }
 
     useEffect(() => {
-    fetchProducts()
+        fetchProducts()
     }, [])
     
     return (
